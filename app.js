afb67ffb55de9e09d8316d1380b76b4d2c5eb77f
JanoiBJR21 <apichat26870@gmail.com>
2024-01-25 02:05:10 +0700

    commit

---
+++ app.js
@@ -0,0 +1,4 @@
+let age = prompt("กรองอายุของคุณ")
+    if(age > 13){
+        document.getElementById("content").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/PGZ7QiKdumo?si=KV1wD7Tl9f4Vuvy2\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
+}
